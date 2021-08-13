a1a9bc27fa23b8289c87d43c77ee102ec6128891
Akshay Dani <akshayvdani@gmail.com>
2021-08-13 11:16:22 +0530

    progress : env file setting secrat key

--- selectme/selectme/settings.py
+++ selectme/selectme/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-^6tvo*1^41=s7zt-v9c4c_d9a-^$ko5nn5f^y6hm85-on+xu6o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
