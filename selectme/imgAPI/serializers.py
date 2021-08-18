930757d6903871d89ea93b112639256139a24837
Akshay Dani <akshayvdani@gmail.com>
2021-08-18 09:37:20 +0530

    progress : eventApi with DB table migrations

--- selectme/imgAPI/serializers.py
+++ selectme/imgAPI/serializers.py
@@ -1,11 +1,18 @@
+from django.db.models import fields
 from rest_framework import serializers
-from .models import post_image
+from .models import post_image, sm_event
+from imgAPI import models
 
 
 class post_image_serializer(serializers.ModelSerializer):
     class Meta:
         model = post_image
         fields = "__all__"
+
+class event_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = sm_event
+        fields = "__all__"        
 
     # def get_date(self, instance):
     #     date = DateTime.datetime.now()
