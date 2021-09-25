a01203751cc54c9d8af66d0297ce23ffc39c2a0b
Akshay Dani <akshayvdani@gmail.com>
2021-09-25 09:13:16 +0530

    Revert "Merge pull request #4 from mrimmortal/SM02-ActiveAndPastOrderAPI"
    This reverts commit 25bfb72efe7f10f30eaf796dfeb3a7453e180c9d, reversing
    changes made to 0ec3f8414d448a74f3e086fd8dbe7112c7d7f40f.

--- selectme/imgAPI/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import *
-
-
-class ImgForm(forms.ModelForm):
-
-    class Meta:
-        model = Image
-        fields = ['title', 'image']
--- selectme/photographer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from photographer.views import *
-
-urlpatterns = [
-    #-----------Past Event-------------
-    path('past-event-list/',PastEventListView.as_view(), name='past-event-list'),   
-    path('past-event/<int:pk>/',RUD_PastEvent.as_view(), name ='past-event')
-    #-----------Past Event-------------
-]
